f44421194d94e9b5848c2e9c283c9681e7756ae6
eynopv <eynopv@gmail.com>
2024-03-26 16:40:39 +0200

    refactor: make internal package

--- gorcli.go
+++ gorcli.go
@@ -7,21 +7,10 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strings"
 	"time"
+
+	"github.com/eynopv/gorcli/internal"
 )
-
-type GorRequest struct {
-	Method string `json:"method"`
-	Path   string `json:"path"`
-	Body   Body   `json:"body"`
-}
-
-type Body map[string]interface{}
-
-type Config struct {
-	Headers map[string]string `json:"headers"`
-}
 
 func main() {
 	showHeaders := flag.Bool("sh", false, "Show response headers")
@@ -34,20 +23,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	config, err := loadConfig()
+	config, err := internal.LoadConfig()
 	if err != nil {
-		return
+		fmt.Println("Unable to load config")
+		os.Exit(1)
 	}
 
+	fmt.Println("Loaded config", config.Loaded)
+	fmt.Println("Config headers", config.Headers["x-api-key"])
+
 	requestName := args[0]
-	request, err := buildRequest(requestName)
+	request, err := internal.NewRequest(requestName)
 	if err != nil {
 		fmt.Println("Unable to make request:", err)
 		return
 	}
 
 	for key, value := range config.Headers {
-		fmt.Println("Have header", key, value)
 		request.Header.Set(key, value)
 	}
 
@@ -99,113 +91,4 @@ func main() {
 	}
 
 	fmt.Println(string(prettyJSON))
-}
-
-func loadRequest(name string) (*GorRequest, error) {
-	var request GorRequest
-
-	filePath := fmt.Sprintf("./collections/%s.json", name)
-	fullPath, err := FullPath(filePath)
-	if err != nil {
-		fmt.Printf("Failed to load file %s: %v\n", *fullPath, err)
-		return nil, err
-	}
-	content, err := os.ReadFile(*fullPath)
-	if err != nil {
-		fmt.Printf("Failed to load file %s: %v\n", *fullPath, err)
-		return nil, err
-	}
-
-	err = json.Unmarshal(content, &request)
-	if err != nil {
-		fmt.Printf("Failed to load file %s: %v\n", *fullPath, err)
-		return nil, err
-	}
-
-	return &request, nil
-}
-
-func buildRequest(name string) (*http.Request, error) {
-	requestData, err := loadRequest(name)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var body *strings.Reader
-	body = nil
-	if requestData.Body != nil {
-		body, err = buildBody(requestData.Body)
-
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	var request *http.Request
-	if body != nil {
-		request, err = http.NewRequest(requestData.Method, requestData.Path, body)
-	} else {
-		request, err = http.NewRequest(requestData.Method, requestData.Path, nil)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return request, nil
-}
-
-func loadConfig() (*Config, error) {
-	var config Config
-
-	configPath := "./gorcli.config.json"
-	filePath, err := FullPath(configPath)
-	if err != nil {
-		fmt.Println("Failed to load config file:", err)
-		return nil, err
-	}
-	configExists := FileExists(*filePath)
-
-	if configExists {
-		content, err := os.ReadFile("./gorcli.config.json")
-		if err != nil {
-			fmt.Println("Failed to load config file:", err)
-			return nil, err
-		}
-
-		err = json.Unmarshal(content, &config)
-		if err != nil {
-			fmt.Println("Failed to load config file:", err)
-			return nil, err
-		}
-
-		for key, value := range config.Headers {
-			config.Headers[key] = parseParameter(value)
-		}
-
-		return &config, nil
-	}
-
-	config = Config{
-		Headers: map[string]string{},
-	}
-
-	return &config, nil
-}
-
-func buildBody(body Body) (*strings.Reader, error) {
-	bodyBytes, err := json.Marshal(body)
-	if err != nil {
-		return nil, err
-	}
-	return strings.NewReader(string(bodyBytes)), nil
-}
-
-func parseParameter(value string) string {
-	if strings.HasPrefix(value, "$env") {
-		parts := strings.Split(value, ".")
-		return os.Getenv(strings.Join(parts[1:], "."))
-	}
-	return value
 }
---
+++ internal/request.go
@@ -0,0 +1,62 @@
+package internal
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Request struct {
+	Method string `json:"method"`
+	Path   string `json:"path"`
+	Body   json.RawMessage
+}
+
+func LoadRequest(name string) (*Request, error) {
+	var request Request
+
+	filePath := fmt.Sprintf("./collections/%s.json", name)
+	fullPath, err := FullPath(filePath)
+	if err != nil {
+		fmt.Printf("Failed to load file %s: %v\n", *fullPath, err)
+		return nil, err
+	}
+
+	content, err := os.ReadFile(*fullPath)
+	if err != nil {
+		fmt.Printf("Failed to load file %s: %v\n", *fullPath, err)
+		return nil, err
+	}
+
+	err = json.Unmarshal(content, &request)
+	if err != nil {
+		fmt.Printf("Failed to load file %s: %v\n", *fullPath, err)
+		return nil, err
+	}
+
+	return &request, nil
+}
+
+func NewRequest(name string) (*http.Request, error) {
+	requestData, err := LoadRequest(name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var request *http.Request
+	if len(requestData.Body) != 0 {
+		bodyReader := bytes.NewReader(requestData.Body)
+		request, err = http.NewRequest(requestData.Method, requestData.Path, bodyReader)
+	} else {
+		request, err = http.NewRequest(requestData.Method, requestData.Path, nil)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return request, nil
+}
