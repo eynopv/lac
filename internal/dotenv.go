ae3a8300c304739d8224ce47164ac643f12ae48a
eynopv <eynopv@gmail.com>
2024-05-11 08:47:13 +0300

    feat: ignore dotenv load error

--- internal/dotenv.go
+++ internal/dotenv.go
@@ -1,14 +1,9 @@
 package internal
 
 import (
-	"fmt"
-
 	"github.com/joho/godotenv"
 )
 
 func LoadDotEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Unable to load dotenv:", err)
-	}
+	godotenv.Load(".env")
 }
