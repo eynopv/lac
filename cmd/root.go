c2217ec0a4f3fc1adeb6a59783a2216a0eedd9b7
eynopv <eynopv@gmail.com>
2024-05-23 15:09:26 +0300

    feat: add support for inline variables and consequently for multiple variables inputs

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/eynopv/lac/internal"
 	"github.com/eynopv/lac/internal/utils"
 	"github.com/spf13/cobra"
@@ -13,9 +16,16 @@ var (
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			internal.LoadDotEnv()
 
-			if len(VariablesFilePath) > 0 {
-				if err := utils.LoadItem(VariablesFilePath, &Variables); err != nil {
-					return err
+			Variables = map[string]string{}
+
+			for _, variableInput := range VariablesInput {
+				err := utils.LoadItem(variableInput, &Variables)
+				if err != nil {
+					keyValue := strings.Split(variableInput, "=")
+					if len(keyValue) != 2 {
+						return fmt.Errorf("Invalid variables input: %v", variableInput)
+					}
+					Variables[keyValue[0]] = keyValue[1]
 				}
 			}
 
@@ -29,12 +39,12 @@ var (
 		},
 	}
 
-	VariablesFilePath string
-	HeadersFilePath   string
-	Verbose           bool
-	Variables         map[string]string
-	Headers           map[string]string
-	Timeout           int
+	VariablesInput  []string
+	HeadersFilePath string
+	Verbose         bool
+	Variables       map[string]string
+	Headers         map[string]string
+	Timeout         int
 )
 
 func Execute() error {
@@ -43,7 +53,7 @@ func Execute() error {
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
-	rootCmd.PersistentFlags().StringVar(&VariablesFilePath, "vars", "", "variables file path")
+	rootCmd.PersistentFlags().StringSliceVar(&VariablesInput, "vars", []string{}, "variables")
 	rootCmd.PersistentFlags().StringVar(&HeadersFilePath, "headers", "", "headers file path")
 	rootCmd.PersistentFlags().IntVarP(&Timeout, "timeout", "t", 15, "request timeout")
 }
